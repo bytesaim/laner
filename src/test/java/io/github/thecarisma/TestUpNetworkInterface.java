86a20c8f6e3bafcb1bac04182f664b6f881cc7e5
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-25 13:57:53 +0100

    creates UpNetworkInterface class andf test

--- src/test/java/io/github/thecarisma/TestUpNetworkInterface.java
+++ src/test/java/io/github/thecarisma/TestUpNetworkInterface.java
@@ -1,4 +1,27 @@
 package io.github.thecarisma;
 
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
 public class TestUpNetworkInterface {
+
+    @Test
+    public void TestGetNetworkInterfaces() throws SocketException {
+        ArrayList<NetworkInterface> networkInterfaces = UpNetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface networkInterface : networkInterfaces) {
+            System.out.println(networkInterface.getDisplayName());
+            System.out.println(networkInterface.getName());
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+                System.out.printf("InetAddress: %s\n", inetAddress);
+            }
+        }
+    }
+
 }
