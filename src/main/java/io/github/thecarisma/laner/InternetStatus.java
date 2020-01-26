0aa58cd0d2b4a2d3f47daf77600fd6a7eb8c5232
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-26 18:18:12 +0100

    create listener for checking internet connection

--- src/main/java/io/github/thecarisma/laner/InternetStatus.java
+++ src/main/java/io/github/thecarisma/laner/InternetStatus.java
@@ -1,16 +1,29 @@
 package io.github.thecarisma.laner;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class InternetStatus implements Runnable {
 
+    private ArrayList<LanerListener> lanerListeners = new ArrayList<>();
     private String urlIp;
-
-    int delayInSeconds = 5;
+    private int delayInSeconds = 5;
+    private Status status = Status.DISCONNECTED;
 
     public InternetStatus(String urlIp) {
         this.urlIp = urlIp;
+    }
+
+    public InternetStatus(String urlIp, LanerListener lanerListener, int delayInSeconds) {
+        this.urlIp = urlIp;
+        this.lanerListeners.add(lanerListener);
+        this.delayInSeconds = delayInSeconds;
+    }
+
+    public InternetStatus(String urlIp, LanerListener lanerListener) {
+        this.urlIp = urlIp;
+        this.lanerListeners.add(lanerListener);
     }
 
     public InternetStatus(String urlIp, int delayInSeconds) {
@@ -20,6 +33,18 @@ public class InternetStatus implements Runnable {
 
     public static boolean IsConnected() {
         return LanerNetworkInterface.isReachable("thecarisma.github.io", 80, 1000);
+    }
+
+    public ArrayList<LanerListener> getLanerListeners() {
+        return lanerListeners;
+    }
+
+    public void addLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public void removeLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.remove(lanerListener);
     }
 
     public boolean isConnected() {
@@ -33,11 +58,21 @@ public class InternetStatus implements Runnable {
             @Override
             public void run() {
                 if (isConnected()) {
-
+                    if (status == Status.DISCONNECTED) {
+                        status = Status.CONNECTED;
+                    }
                 } else {
-
+                    if (status == Status.CONNECTED) {
+                        status = Status.DISCONNECTED;
+                    }
                 }
             }
         }, 0, (delayInSeconds * 1000));
     }
+
+    public enum Status {
+        CONNECTED,
+        DISCONNECTED
+    }
+
 }
