fb04e84af5a993029e03af97734cbce679ce6fce
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-26 18:08:42 +0100

    add isConnected static method to check internet connection

--- src/main/java/io/github/thecarisma/laner/InternetStatus.java
+++ src/main/java/io/github/thecarisma/laner/InternetStatus.java
@@ -1,4 +1,19 @@
 package io.github.thecarisma.laner;
 
-public class InternetStatus {
+public class InternetStatus implements Runnable {
+
+    private String urlIp;
+
+    public InternetStatus(String urlIp) {
+        this.urlIp = urlIp;
+    }
+
+    public static boolean isConnected() {
+        return LanerNetworkInterface.isReachable("thecarisma.github.io", 80, 1000);
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
---
+++ src/test/java/io/github/thecarisma/laner/TestInternetStatus.java
@@ -0,0 +1,12 @@
+package io.github.thecarisma.laner;
+
+import org.junit.Test;
+
+public class TestInternetStatus {
+
+    @Test
+    public void TestIsConnected() {
+        System.out.println(InternetStatus.isConnected());
+    }
+
+}
