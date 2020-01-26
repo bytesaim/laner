2d1952551210207ae6dcd4aaddfdbd9352076a7c
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-26 18:11:53 +0100

    create listener for checking internet connection

--- src/main/java/io/github/thecarisma/laner/InternetStatus.java
+++ src/main/java/io/github/thecarisma/laner/InternetStatus.java
@@ -1,11 +1,21 @@
 package io.github.thecarisma.laner;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class InternetStatus implements Runnable {
 
     private String urlIp;
 
+    int delayInSeconds = 5;
+
     public InternetStatus(String urlIp) {
         this.urlIp = urlIp;
+    }
+
+    public InternetStatus(String urlIp, int delayInSeconds) {
+        this.urlIp = urlIp;
+        this.delayInSeconds = delayInSeconds;
     }
 
     public static boolean isConnected() {
@@ -14,6 +24,12 @@ public class InternetStatus implements Runnable {
 
     @Override
     public void run() {
-
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                
+            }
+        }, 0, (delayInSeconds * 1000));
     }
 }
