9a51bb1656e66cb5d796e99e7f152ce7d5e57684
Azeez Adewale <azeezadewale98@gmail.com>
2020-01-28 16:11:10 +0100

    creates LAN and ETHERNET Listener

---
+++ src/main/java/io/github/thecarisma/laner/EthernetStatus.java
@@ -0,0 +1,104 @@
+package io.github.thecarisma.laner;
+
+import io.github.thecarisma.util.TRunnable;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+//TODO: listen for ethernet
+public class EthernetStatus implements TRunnable {
+
+    private ArrayList<LanerListener> lanerListeners = new ArrayList<>();
+    private String urlIp = "thecarisma.github.io";
+    private int delayInSeconds = 1;
+    private Status status = Status.DISCONNECTED;
+    private Timer timer;
+
+    public EthernetStatus(String urlIp) {
+        this.urlIp = urlIp;
+    }
+
+    public EthernetStatus(String urlIp, LanerListener lanerListener, int delayInSeconds) {
+        this.urlIp = urlIp;
+        this.lanerListeners.add(lanerListener);
+        this.delayInSeconds = delayInSeconds;
+    }
+
+    public EthernetStatus(String urlIp, LanerListener lanerListener) {
+        this.urlIp = urlIp;
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public EthernetStatus(String urlIp, int delayInSeconds) {
+        this.urlIp = urlIp;
+        this.delayInSeconds = delayInSeconds;
+    }
+
+    public EthernetStatus(LanerListener lanerListener) {
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public EthernetStatus(LanerListener lanerListener, int delayInSeconds) {
+        this.lanerListeners.add(lanerListener);
+        this.delayInSeconds = delayInSeconds;
+    }
+
+    public static boolean IsConnected() {
+        return LanerNetworkInterface.isReachable("thecarisma.github.io", 80, 1000);
+    }
+
+    public ArrayList<LanerListener> getLanerListeners() {
+        return lanerListeners;
+    }
+
+    public void addLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public void removeLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.remove(lanerListener);
+    }
+
+    public boolean isConnected() {
+        return LanerNetworkInterface.isReachable(urlIp, 80, 1000);
+    }
+
+    @Override
+    public void run() {
+        broadcastToListeners(status);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (isConnected()) {
+                    if (status == Status.DISCONNECTED) {
+                        status = Status.CONNECTED;
+                        broadcastToListeners(status);
+                    }
+                } else {
+                    if (status == Status.CONNECTED) {
+                        status = Status.DISCONNECTED;
+                        broadcastToListeners(status);
+                    }
+                }
+            }
+        }, 0, (delayInSeconds * 1000));
+    }
+
+    private void broadcastToListeners(Object o) {
+        for (LanerListener lanerListener : lanerListeners) {
+            lanerListener.report(o);
+        }
+    }
+
+    public void stop() {
+        timer.cancel();
+    }
+
+    public enum Status {
+        CONNECTED,
+        DISCONNECTED
+    }
+
+}
---
+++ src/main/java/io/github/thecarisma/laner/LanStatus.java
@@ -0,0 +1,104 @@
+package io.github.thecarisma.laner;
+
+import io.github.thecarisma.util.TRunnable;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+//TODO: listen for lan
+public class LanStatus implements TRunnable {
+
+    private ArrayList<LanerListener> lanerListeners = new ArrayList<>();
+    private String urlIp = "thecarisma.github.io";
+    private int delayInSeconds = 1;
+    private Status status = Status.DISCONNECTED;
+    private Timer timer;
+
+    public LanStatus(String urlIp) {
+        this.urlIp = urlIp;
+    }
+
+    public LanStatus(String urlIp, LanerListener lanerListener, int delayInSeconds) {
+        this.urlIp = urlIp;
+        this.lanerListeners.add(lanerListener);
+        this.delayInSeconds = delayInSeconds;
+    }
+
+    public LanStatus(String urlIp, LanerListener lanerListener) {
+        this.urlIp = urlIp;
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public LanStatus(String urlIp, int delayInSeconds) {
+        this.urlIp = urlIp;
+        this.delayInSeconds = delayInSeconds;
+    }
+
+    public LanStatus(LanerListener lanerListener) {
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public LanStatus(LanerListener lanerListener, int delayInSeconds) {
+        this.lanerListeners.add(lanerListener);
+        this.delayInSeconds = delayInSeconds;
+    }
+
+    public static boolean IsConnected() {
+        return LanerNetworkInterface.isReachable("thecarisma.github.io", 80, 1000);
+    }
+
+    public ArrayList<LanerListener> getLanerListeners() {
+        return lanerListeners;
+    }
+
+    public void addLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public void removeLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.remove(lanerListener);
+    }
+
+    public boolean isConnected() {
+        return LanerNetworkInterface.isReachable(urlIp, 80, 1000);
+    }
+
+    @Override
+    public void run() {
+        broadcastToListeners(status);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (isConnected()) {
+                    if (status == Status.DISCONNECTED) {
+                        status = Status.CONNECTED;
+                        broadcastToListeners(status);
+                    }
+                } else {
+                    if (status == Status.CONNECTED) {
+                        status = Status.DISCONNECTED;
+                        broadcastToListeners(status);
+                    }
+                }
+            }
+        }, 0, (delayInSeconds * 1000));
+    }
+
+    private void broadcastToListeners(Object o) {
+        for (LanerListener lanerListener : lanerListeners) {
+            lanerListener.report(o);
+        }
+    }
+
+    public void stop() {
+        timer.cancel();
+    }
+
+    public enum Status {
+        CONNECTED,
+        DISCONNECTED
+    }
+
+}
