a62d66465276fdc232a87daeecb46aad9518bbad
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-28 06:57:08 +0100

    implement server and client functionality

---
+++ src/main/java/io/github/thecarisma/laner/LanerPrintWriter.java
@@ -0,0 +1,29 @@
+package io.github.thecarisma.laner;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+public class LanerPrintWriter extends PrintWriter {
+
+    public LanerPrintWriter(PrintWriter writer) {
+        super(writer);
+    }
+
+    public LanerPrintWriter(OutputStream out) {
+        super(out);
+    }
+
+    public LanerPrintWriter(PrintWriter writer, boolean autoFlush) {
+        super(writer, autoFlush);
+    }
+
+    public LanerPrintWriter(OutputStream out, boolean autoFlush) {
+        super(out, autoFlush);
+    }
+
+
+    public boolean isOpen() {
+        return out != null;
+    }
+
+}
--- src/main/java/io/github/thecarisma/laner/LanerServer.java
+++ src/main/java/io/github/thecarisma/laner/LanerServer.java
@@ -1,8 +1,12 @@
 package io.github.thecarisma.laner;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
@@ -13,8 +17,7 @@ public class LanerServer implements Runnable {
     String ipAddress;
     int port;
     int backlog = 50;
-    private boolean serverStarted = false;
-    private boolean stopServer = false;
+    private boolean isRunning = false;
 
     public LanerServer(String ipAddress, int port, int backlog) {
         this.ipAddress = ipAddress;
@@ -77,15 +80,66 @@ public class LanerServer implements Runnable {
 
     @Override
     public void run() {
-        if (!serverStarted) {
-            startServer();
+        startServer();
+        try {
+            while (isRunning) {
+                Socket clientSocket = null;
+                try {
+                    clientSocket = serverSocket.accept();
+                } catch (IOException e) {
+                    System.err.println("Accept failed.");
+                    System.exit(1);
+                }
+                LanerPrintWriter out = new LanerPrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String inputLine, outputLine;
+                broadcastToListeners(new LanerServerStream(out, in));
+                KnockKnockProtocol kkp = new KnockKnockProtocol();
+
+                outputLine = kkp.processInput(null);
+                out.write("HTTP/1.0 200 OK\r\n");
+                out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+                out.write("Server: Apache/0.8.4\r\n");
+                out.write("Content-Type: text/html\r\n");
+                out.write("Content-Length: 59\r\n");
+                out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
+                out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+                out.write("\r\n");
+                out.write("<html><head><title>Exemple</title></head>");
+                out.write("<body>Ceci est une page d'exemple.</body></html>");
+                //out.write(outputLine);
+
+                while ((inputLine = in.readLine()) != null) {
+                    //outputLine = kkp.processInput(inputLine);
+                    //out.println(outputLine);
+                    //if (outputLine.equals("Bye."))
+                    //    break;
+                    //System.out.println(inputLine);
+                    break;
+                }
+                if (out.isOpen()) {
+                    out.close();
+                }
+                if (in.isOpen()) {
+                    in.close();
+                }
+                clientSocket.close();
+            }
+        } catch (IOException ex) {
+            //broadcastToListeners(Object o);
+            ex.printStackTrace();
+            try {
+                stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private void startServer() {
         try {
             serverSocket = new ServerSocket(port, backlog, InetAddress.getByName(ipAddress));
-            serverStarted = true;
+            isRunning = true;
         } catch (IOException e) {
             //broadcastToListeners(Object o);
             e.printStackTrace();
@@ -93,11 +147,16 @@ public class LanerServer implements Runnable {
 
     }
 
-    public void stop() {
-        stopServer = true;
+    public void stop() throws IOException {
         //possible send a closing byte to the server to
         //initiate immediate closing
-        LanerNetworkInterface.isReachable(ipAddress, port, 100);
+        if (isRunning) {
+            LanerNetworkInterface.isReachable(ipAddress, port, 100);
+            if (serverSocket != null) {
+                serverSocket.close();
+                isRunning = false;
+            }
+        }
     }
 
     private void broadcastToListeners(Object o) {
@@ -105,4 +164,15 @@ public class LanerServer implements Runnable {
             lanerListener.report(o);
         }
     }
+
+    public static class LanerServerStream {
+        public PrintWriter out;
+        public BufferedReader in;
+
+        public LanerServerStream(PrintWriter out, BufferedReader in) {
+            this.out = out;
+            this.in = in;
+        }
+    }
+
 }
