4ed6f263ddebff2409192a99799939fb5d17e089
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-28 21:34:55 +0100

    create test for ThreadsManager class

--- src/test/java/io/github/thecarisma/util/TestThreadsManager.java
+++ src/test/java/io/github/thecarisma/util/TestThreadsManager.java
@@ -1,5 +1,6 @@
 package io.github.thecarisma.util;
 
+import io.github.thecarisma.laner.InternetStatus;
 import io.github.thecarisma.laner.LanerListener;
 import io.github.thecarisma.laner.LanerNetworkInterface;
 import io.github.thecarisma.laner.NetworkDevices;
@@ -19,6 +20,49 @@ public class TestThreadsManager {
                 if (o instanceof NetworkDevices.NetworkDevice) {
                     if (index[0] > 0) {
                         try {
+                            System.out.println("Killing all the thread");
+                            threadsManager.killAll();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(o);
+                    index[0]++;
+                }
+            }
+        });
+        threadsManager.registerTRunnable("testnetworddevices", nd);
+        nd.run();
+    }
+
+    @Test
+    public void Test2() throws UnknownHostException {
+        final ThreadsManager threadsManager = new ThreadsManager();
+        final int[] index = {0};
+        NetworkDevices nd = new NetworkDevices(LanerNetworkInterface.getIPV4Address(), new LanerListener() {
+            @Override
+            public void report(Object o) {
+                if (o instanceof NetworkDevices.NetworkDevice) {
+                    if (index[0] > 0) {
+                        try {
+                            System.out.println("Killing all the thread");
+                            threadsManager.killAll();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(o);
+                    index[0]++;
+                }
+            }
+        });
+        InternetStatus is = new InternetStatus("thecarisma.github.io", new LanerListener() {
+            @Override
+            public void report(Object o) {
+                if (o instanceof InternetStatus.Status) {
+                    if (index[0] > 0) {
+                        try {
+                            System.out.println("Killing all the thread");
                             threadsManager.killAll();
                         } catch (Exception e) {
                             e.printStackTrace();
