a76d77691de25e98dd5a09ee0f0e2cd11a689cbb
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-28 21:31:21 +0100

    creeate test for ThreadsManager class

--- src/test/java/io/github/thecarisma/util/TestThreadsManager.java
+++ src/test/java/io/github/thecarisma/util/TestThreadsManager.java
@@ -1,4 +1,34 @@
 package io.github.thecarisma.util;
 
+import io.github.thecarisma.laner.LanerListener;
+import io.github.thecarisma.laner.LanerNetworkInterface;
+import io.github.thecarisma.laner.NetworkDevices;
+
+import java.net.UnknownHostException;
+
 public class TestThreadsManager {
+
+    public static void main(String[] args) throws UnknownHostException {
+        final ThreadsManager threadsManager = new ThreadsManager();
+        final int[] index = {0};
+        NetworkDevices nd = new NetworkDevices(LanerNetworkInterface.getIPV4Address(), new LanerListener() {
+            @Override
+            public void report(Object o) {
+                if (o instanceof NetworkDevices.NetworkDevice) {
+                    if (index[0] > 0) {
+                        try {
+                            threadsManager.killAll();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(o);
+                    index[0]++;
+                }
+            }
+        });
+        threadsManager.registerTRunnable("testnetworddevices", nd);
+        nd.run();
+    }
+
 }
