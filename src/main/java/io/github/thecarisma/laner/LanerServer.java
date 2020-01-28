bf8551be5a6a3fbf7328183d1b57c95cb5ad6dc0
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-28 06:38:17 +0100

    implement server and client functionality

--- src/main/java/io/github/thecarisma/laner/LanerServer.java
+++ src/main/java/io/github/thecarisma/laner/LanerServer.java
@@ -2,9 +2,11 @@ package io.github.thecarisma.laner;
 
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
-public class LanerServer {
+public class LanerServer implements Runnable {
 
+    private ArrayList<LanerListener> lanerListeners = new ArrayList<>();
     ServerSocket serverSocket;
     String ipAddress;
     int port;
@@ -16,9 +18,22 @@ public class LanerServer {
         this.backlog = backlog;
     }
 
+    public LanerServer(String ipAddress, int port, int backlog, LanerListener lanerListener) {
+        this.ipAddress = ipAddress;
+        this.port = port;
+        this.backlog = backlog;
+        this.lanerListeners.add(lanerListener);
+    }
+
     public LanerServer(String ipAddress, int port) {
         this.ipAddress = ipAddress;
         this.port = port;
+    }
+
+    public LanerServer(String ipAddress, int port, LanerListener lanerListener) {
+        this.ipAddress = ipAddress;
+        this.port = port;
+        this.lanerListeners.add(lanerListener);
     }
 
     public LanerServer(int port) throws UnknownHostException {
@@ -26,4 +41,44 @@ public class LanerServer {
         this.port = port;
     }
 
+    public LanerServer(int port, LanerListener lanerListener) throws UnknownHostException {
+        this.ipAddress = LanerNetworkInterface.getIPV4Address();
+        this.port = port;
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public ArrayList<LanerListener> getLanerListeners() {
+        return lanerListeners;
+    }
+
+    public void addLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.add(lanerListener);
+    }
+
+    public void removeLanerListener(LanerListener lanerListener) {
+        this.lanerListeners.remove(lanerListener);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    private void broadcastToListeners(Object o) {
+        for (LanerListener lanerListener : lanerListeners) {
+            lanerListener.report(o);
+        }
+    }
 }
--- src/test/java/io/github/thecarisma/laner/TestLanerServer.java
+++ src/test/java/io/github/thecarisma/laner/TestLanerServer.java
@@ -6,7 +6,13 @@ public class TestLanerServer {
 
     @Test
     public void Test1() {
+        LanerServer lanerServer = new LanerServer("192.168.8.102", 2020, new LanerListener() {
+            @Override
+            public void report(Object o) {
 
+            }
+        });
+        lanerServer.run();
     }
 
 }
