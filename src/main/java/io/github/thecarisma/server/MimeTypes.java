572f03d18f08e4a2af75d45d58bfdefbe2c042ee
Azeez Adewale <azeezadewale98@gmail.com>
2020-02-07 13:27:47 +0100

    creates the MimeTypes class fo and test find and detect mime type

--- src/main/java/io/github/thecarisma/server/MimeTypes.java
+++ src/main/java/io/github/thecarisma/server/MimeTypes.java
@@ -9,13 +9,19 @@ public class MimeTypes {
 
     public static String THREE_DM = "x-world/x-3dmf";
 
+    public static String CSS = "text/css";
+
+    public static String CSS_APP = "application/x-pointplus";
+
     public static String TEXT_HTML = "text/html";
 
     public static String getMimeType(String fileName) {
-        String ext = fileName.substring(fileName.lastIndexOf('.'), fileName.length());
+        String ext = fileName.substring(fileName.lastIndexOf('.') + 1);
         switch (ext) {
-            case ".3dm":
+            case "3dm":
                 return THREE_DM;
+            case "css":
+                return CSS;
             case "html":
             default:
                 return TEXT_HTML;
--- src/test/java/io/github/thecarisma/server/TestMimeTypes.java
+++ src/test/java/io/github/thecarisma/server/TestMimeTypes.java
@@ -1,12 +1,24 @@
 package io.github.thecarisma.server;
 
-import javafx.fxml.FXML;
+import org.junit.Test;
 
 public class TestMimeTypes {
 
-    @FXML
+    @Test
     public void Test1() {
-        
+        String[] names = { "text.html", "css", ".", "avery.try.abc", "", "..." } ;
+        for (String name : names) {
+            String ext = name.substring(name.lastIndexOf('.') + 1, name.length());
+            System.out.println(ext);
+        }
+    }
+
+    @Test
+    public void Test2() {
+        String[] names = { "file.3dm", "text.html", "css", ".", "avery.try.abc", "", "..." } ;
+        for (String name : names) {
+            System.out.println(MimeTypes.getMimeType(name));
+        }
     }
 
 }
