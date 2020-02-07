c5a4eb149c335ffe392acd9af854423a65e754ed
Adewale Azeez <azeezadewale98@gmail.com>
2020-02-07 21:26:37 +0100

    add more type fields to MimeTypes

--- src/main/java/io/github/thecarisma/server/MimeTypes.java
+++ src/main/java/io/github/thecarisma/server/MimeTypes.java
@@ -7,343 +7,347 @@ package io.github.thecarisma.server;
  */
 public class MimeTypes {
 
-    public static String THREE_DM = "x-world/x-3dmf";
+    public static final String THREE_DM = "x-world/x-3dmf";
 
-    public static String THREE_DMF = "x-world/x-3dmf";
+    public static final String THREE_DMF = "x-world/x-3dmf";
 
-    public static String A = "application/octet-stream";
+    public static final String A = "application/octet-stream";
 
-    public static String AAB = "application/x-authorware-bin";
+    public static final String AAB = "application/x-authorware-bin";
 
-    public static String AAM = "application/x-authorware-map";
+    public static final String AAM = "application/x-authorware-map";
 
-    public static String AAS = "application/x-authorware-seg";
+    public static final String AAS = "application/x-authorware-seg";
 
-    public static String ABC = "text/vnd.abc";
+    public static final String ABC = "text/vnd.abc";
 
-    public static String ACGI = "text/html";
+    public static final String ACGI = "text/html";
 
-    public static String AFL = "video/animaflex";
+    public static final String AFL = "video/animaflex";
 
-    public static String AI = "application/postscript";
+    public static final String AI = "application/postscript";
 
-    public static String AIF = "audio/aiff";
+    public static final String AIF = "audio/aiff";
 
-    public static String AIF_X = "audio/x-aiff";
+    public static final String AIF_X = "audio/x-aiff";
 
-    public static String AIFC = "audio/aiff";
+    public static final String AIFC = "audio/aiff";
 
-    public static String AIFC_X = "audio/x-aiff";
+    public static final String AIFC_X = "audio/x-aiff";
 
-    public static String AIFF = "audio/aiff";
+    public static final String AIFF = "audio/aiff";
 
-    public static String AIFF_X = "audio/x-aiff";
+    public static final String AIFF_X = "audio/x-aiff";
 
-    public static String AIM = "application/x-aim";
+    public static final String AIM = "application/x-aim";
 
-    public static String AIP = "text/x-audiosoft-intra";
+    public static final String AIP = "text/x-audiosoft-intra";
 
-    public static String ANI = "application/x-navi-animation";
+    public static final String ANI = "application/x-navi-animation";
 
-    public static String AOS = "application/x-nokia-9000-communicator-add-on-software";
+    public static final String AOS = "application/x-nokia-9000-communicator-add-on-software";
 
-    public static String APS = "application/mime";
+    public static final String APS = "application/mime";
 
-    public static String ARC = "application/octet-stream";
+    public static final String ARC = "application/octet-stream";
 
-    public static String ARJ = "application/arj";
+    public static final String ARJ = "application/arj";
 
-    public static String ARJ_OCTET = "application/octet-stream";
+    public static final String ARJ_OCTET = "application/octet-stream";
 
-    public static String ART = "image/x-jg";
+    public static final String ART = "image/x-jg";
 
-    public static String ASF = "video/x-ms-asf";
+    public static final String ASF = "video/x-ms-asf";
 
-    public static String ASM = "text/x-asm";
+    public static final String ASM = "text/x-asm";
 
-    public static String ASP = "text/asp";
+    public static final String ASP = "text/asp";
 
-    public static String ASX = "application/x-mplayer2";
+    public static final String ASX = "application/x-mplayer2";
 
-    public static String ASX_X = "video/x-ms-asf";
+    public static final String ASX_X = "video/x-ms-asf";
 
-    public static String ASX_X_PLUGIN = "video/x-ms-asf-plugin";
+    public static final String ASX_X_PLUGIN = "video/x-ms-asf-plugin";
 
-    public static String AU = "audio/basic";
+    public static final String AU = "audio/basic";
 
-    public static String AU_X = "audio/x-au";
+    public static final String AU_X = "audio/x-au";
 
-    public static String AVI_APP = "application/x-troff-msvideo";
+    public static final String AVI_APP = "application/x-troff-msvideo";
 
-    public static String AVI = "video/avi";
+    public static final String AVI = "video/avi";
 
-    public static String AVI_MSVIDEO = "video/msvideo";
+    public static final String AVI_MSVIDEO = "video/msvideo";
 
-    public static String AVI_X_MSVIDEO = "video/x-msvideo";
+    public static final String AVI_X_MSVIDEO = "video/x-msvideo";
 
-    public static String AVS = "video/avs-video";
+    public static final String AVS = "video/avs-video";
 
-    public static String BCPIO = "application/x-bcpio";
+    public static final String BCPIO = "application/x-bcpio";
 
-    public static String BIN = "application/mac-binary";
+    public static final String BIN = "application/mac-binary";
 
-    public static String BIN_MAC = "application/macbinary";
+    public static final String BIN_MAC = "application/macbinary";
 
-    public static String BIN_OCTET = "application/octet-stream";
+    public static final String BIN_OCTET = "application/octet-stream";
 
-    public static String BIN_X = "application/x-binary";
+    public static final String BIN_X = "application/x-binary";
 
-    public static String BIN_X_MAC = "application/x-macbinary";
+    public static final String BIN_X_MAC = "application/x-macbinary";
 
-    public static String BM = "image/bmp";
+    public static final String BM = "image/bmp";
 
-    public static String BMP = "image/bmp";
+    public static final String BMP = "image/bmp";
 
-    public static String BMP_X = "image/x-windows-bmp";
+    public static final String BMP_X = "image/x-windows-bmp";
 
-    public static String BOO = "application/book";
+    public static final String BOO = "application/book";
 
-    public static String BOOK = "application/book";
+    public static final String BOOK = "application/book";
 
-    public static String BOZ = "application/x-bzip2";
+    public static final String BOZ = "application/x-bzip2";
 
-    public static String BSH = "application/x-bsh";
+    public static final String BSH = "application/x-bsh";
 
-    public static String BZ = "application/x-bzip";
+    public static final String BZ = "application/x-bzip";
 
-    public static String BZ2 = "application/x-bzip2";
+    public static final String BZ2 = "application/x-bzip2";
 
-    public static String C = "text/plain";
+    public static final String C = "text/plain";
 
-    public static String C_X = "text/x-c";
+    public static final String C_X = "text/x-c";
 
-    public static String CPP = "text/plain";
+    public static final String CPP = "text/plain";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    //public static String BZ2 = "";
+    //public static final String BZ2 = "";
 
-    public static String CSS = "text/css";
+    public static final String MP3 = "audio/mpeg3";
 
-    public static String CSS_APP = "application/x-pointplus";
+    public static final String CSS = "text/css";
 
-    public static String TEXT_HTML = "text/html";
+    public static final String CSS_APP = "application/x-pointplus";
 
-    public static String getMimeType(String fileName) {
+    public static final String TEXT_HTML = "text/html";
+
+    public static final String getMimeType(String fileName) {
         String ext = fileName.substring(fileName.lastIndexOf('.') + 1);
         switch (ext) {
             case "3dm":
                 return THREE_DM;
+            case "mp3":
+                return MP3;
             case "css":
                 return CSS;
             case "html":
