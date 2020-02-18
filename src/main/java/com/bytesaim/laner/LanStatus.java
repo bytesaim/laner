f6267029ec1565378bd653ce0ab331ba169156ff
Adewale Azeez <azeezadewale98@gmail.com>
2020-02-18 07:22:26 +0100

    listen to network interface by name

--- src/main/java/com/bytesaim/laner/LanStatus.java
+++ src/main/java/com/bytesaim/laner/LanStatus.java
@@ -5,41 +5,18 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
 
-public class LanStatus extends EthernetStatus {
+public class LanStatus extends NetworkInterfaceStatusByName {
 
     public LanStatus(LanerListener lanerListener, int delayInSeconds) {
-        super(lanerListener, delayInSeconds);
+        super(lanerListener, delayInSeconds, "eth", "wlan");
     }
 
     public LanStatus(int delayInSeconds) {
-        super(delayInSeconds);
+        super(delayInSeconds, "eth", "wlan");
     }
 
     public LanStatus(LanerListener lanerListener) {
-        super(lanerListener);
-    }
-
-    @Override
-    protected boolean isConnected() throws SocketException {
-        boolean containsEth = false;
-        ArrayList<NetworkInterface> networkInterfaces =  LanerNetworkInterface.getNetworkInterfacesNoLoopback();
-        for (NetworkInterface networkInterface : networkInterfaces) {
-            if (networkInterface.getName().startsWith("eth") || networkInterface.getName().startsWith("wlan")) {
-                if (!this.networkInterfaceIPV4Address.isEmpty()) {
-                    ArrayList<InetAddress> addresses = LanerNetworkInterface.getInetAddresses(networkInterface);
-                    for (InetAddress address : addresses) {
-                        if (this.networkInterfaceIPV4Address.equals(address.getHostAddress())) {
-                            containsEth = true;
-                            break;
-                        }
-                    }
-                } else {
-                    containsEth = true;
-                    break;
-                }
-            }
-        }
-        return containsEth;
+        super(lanerListener, "eth", "wlan");
     }
 
 
